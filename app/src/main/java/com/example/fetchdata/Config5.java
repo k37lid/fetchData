f025743731de027d9fa9f990eae05ca853467b5c
k37lid <202392413@student.ttcollege.edu.sa>
2019-11-28 03:39:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fetchdata/Config5.java
@@ -0,0 +1,10 @@
+package com.example.fetchdata;
+
+public class Config5 {
+    public static final String DATA_URL = "http://172.20.10.4:80/sqli/fetch.php?email=";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PHONE = "phone";
+    public static final String KEY_NAME =  "name";
+    public static final String KEY_ID =  "id";
+    public static final String JSON_ARRAY = "result";
+}
